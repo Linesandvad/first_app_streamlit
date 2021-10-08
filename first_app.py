7fbb91f93c6afa0665cc77ce80e1d5d9a9a16231
Linesandvad <80422536+Linesandvad@users.noreply.github.com>
2021-10-08 12:04:45 +0200

    Add files via upload

---
+++ first_app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import streamlit as st
+import numpy as np 
+import pandas as pd
+
+#Title of webpage
+st.title("Welcome!")
+
+#Subtitle
+st.header("This is a subtitle")
+
+#Subsubtitle
+st.subheader("This is a subsubtitle")
+
+st.text("Pure text")
+
+st.markdown("Markdown format")
+st.markdown("'Hello, *World!* :sunglasses:")
+
+#st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+#st.session_state.name
+
+
+pets = ["Dog", "Cat", "Mouse"]
+
+pet = st.radio("Pick a pet", pets)
+
+st.text(pet)
+
+st.line_chart([1,2,3])
