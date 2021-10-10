58f698ff14bca4a6d8a46f93b05baac9a6409bb5
Linesandvad <80422536+Linesandvad@users.noreply.github.com>
2021-10-10 10:40:54 +0200

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 
 import streamlit as st
-import numpy as np 
-import pandas as pd
+#import numpy as np 
+#import pandas as pd
 
 #Title of webpage
 st.title("Welcome!")
 
 #Subtitle
-st.header("This is a subtitle")
+#st.header("This is a subtitle")
 
 #Subsubtitle
-st.subheader("This is a subsubtitle")
+#st.subheader("This is a subsubtitle")
 
-st.text("Pure text")
+#st.text("Pure text")
 
-st.markdown("Markdown format")
-st.markdown("'Hello, *World!* :sunglasses:")
+#st.markdown("Markdown format")
+#st.markdown("'Hello, *World!* :sunglasses:")
 
 #st.text_input("Your name", key="name")
 
 # You can access the value at any point with:
 #st.session_state.name
 
+#pets = ["Dog", "Cat", "Mouse"]
 
-pets = ["Dog", "Cat", "Mouse"]
+#pet = st.radio("Pick a pet", pets)
 
-pet = st.radio("Pick a pet", pets)
+#st.text(pet)
 
-st.text(pet)
-
-st.line_chart([1,2,3])
+#st.line_chart([1,2,3])
